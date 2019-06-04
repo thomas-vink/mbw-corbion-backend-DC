f07d871eed1ed8097d350dfb9ee65c78d4d4078d
thomas <99040159@mydavinci.nl>
2019-06-04 09:43:08 +0200

    scanpoints begin

---
+++ app/Http/Controllers/ScanpointController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Scanpoint;
+use Illuminate\Http\Request;
+
+class ScanpointController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('scanpoints.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Scanpoint  $scanpoint
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Scanpoint $scanpoint)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Scanpoint  $scanpoint
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Scanpoint $scanpoint)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Scanpoint  $scanpoint
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Scanpoint $scanpoint)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Scanpoint  $scanpoint
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Scanpoint $scanpoint)
+    {
+        //
+    }
+}
