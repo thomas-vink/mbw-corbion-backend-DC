c60850a91e18d8b73a812ffa1a9054f79dd8cef8
thomas-vink <99040159@mydavinci.nl>
2019-07-08 17:32:31 +0200

    fixd bugs

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -1,9 +1,6 @@
 <?php
-
 namespace App\Http\Middleware;
-
 use Closure;
-
 class CheckRole
 {
     /**
@@ -13,15 +10,17 @@ class CheckRole
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $role)
     {
-
-        $user = $request->user();
-
-        if($user->is_admin === 0) {
+        if($request->user()->role->name === 'Shiftmanager'){
+            return $next($request);
+        }
+        if($request->user()->role->name != $role){
             return redirect()->back()->with('error', 'Login met een shiftmanager account om dit aan te kunnen passen.');
         }
-
+        if($request->user()->role->name === 'Operator') {
+            return redirect()->back()->with('error', 'Login met een shiftmanager of spillteam account om dit aan te kunnen passen.');
+        }
         return $next($request);
     }
 }
